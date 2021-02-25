c8e3ba573e2c9ef0d3046b2a886135f505841b41
Patryk-Kowalczyk <tojestkowala@gmail.com>
2021-02-25 17:41:57 +0100

    Hosts

--- cottages/settings.py
+++ cottages/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['domki-hosting.usermd.net', 'localhost', '127.0.0.1']
 
 
 # Application definition
