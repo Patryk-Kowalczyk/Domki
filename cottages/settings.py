f1017800ce4f41446657a5ca27352d5ab195af3a
Patryk-Kowalczyk <tojestkowala@gmail.com>
2021-02-25 18:00:04 +0100

    Settings V3

--- cottages/settings.py
+++ cottages/settings.py
@@ -130,11 +130,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-
-
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = 'media/'
+if MYDEVIL:
+    MEDIA_ROOT = 'public/media/'
+
 STATIC_ROOT = 'static/'
 
 if MYDEVIL:
